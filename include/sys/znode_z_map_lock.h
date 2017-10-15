a869eed76f28324fec95039ec65e76ba0f2989b3
Sean Doran <smd@use.net>
2017-10-15 19:31:24 +0100

    more 0 % x => true for all x fixing

--- include/sys/znode_z_map_lock.h
+++ include/sys/znode_z_map_lock.h
@@ -42,9 +42,10 @@ z_map_upgrade_lock(znode_t *zp, boolean_t *need_release, boolean_t *need_upgrade
 	if (*need_upgrade != B_TRUE)
 		return(lock_tries);
 
-	for (unsigned int i = 0; !rw_tryupgrade(&zp->z_map_lock); i++) {
+	/* we start at 1 to skip 0 % x => true for all x */
+	for (unsigned int i = 1; !rw_tryupgrade(&zp->z_map_lock); i++) {
 		lock_tries++;
-		if (i > 0 && (i % 512) == 0)
+		if ((i % 512) == 0)
 			printf("ZFS: %s: trying to upgrade z_map_lock (%d) for %s (held by %s)\n",
 			    __func__, i, caller,
 			    (zp->z_map_lock_holder != NULL)
@@ -74,7 +75,9 @@ z_map_rw_lock(znode_t *zp, boolean_t *need_release, boolean_t *need_upgrade, con
 		*need_upgrade = B_FALSE;
 		return (lock_tries);
 	}
-	for (unsigned int i=0; !rw_tryenter(&zp->z_map_lock, RW_WRITER) ; i++) {
+
+	/* we start at 1 to skip 0 % x => true for all x */
+	for (unsigned int i=1; !rw_tryenter(&zp->z_map_lock, RW_WRITER) ; i++) {
 		lock_tries++;
 		if (i > 0 && (i % 512) == 0)
 			printf("ZFS: %s: waiting for z_map_lock (%u) for %s (held by %s)\n",
