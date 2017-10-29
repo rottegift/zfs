d92f38c3ae8f97569d48ffc07dc6f58f8ee8117f
Sean Doran <smd@use.net>
2017-10-29 20:05:21 +0000

    noisy ASSERTs
    one is simply not going to be happy, removed it
    
    two give
    
    Oct 29 20:05:37 mis kernel[0]: zfs_vnops.c mappedread 746 : ASSERT3( uio_resid(uio) <= zp->z_size) failed (15 <= 5)
    Oct 29 20:05:37 mis kernel[0]: zfs_vnops.c mappedread 747
    : ASSERT3( uio_resid(uio) + uio_offset(uio) <= zp->z_size)
    failed (15 <= 5)
    
    regularly, which is odd.

--- module/zfs/zfs_vnops.c
+++ module/zfs/zfs_vnops.c
@@ -743,8 +743,8 @@ mappedread(vnode_t *vp, int nbytes, struct uio *uio)
 
     ASSERT3S(nbytes, >, 0);
     ASSERT3S(uio_offset(uio), <=, zp->z_size); // offset is within file
-    ASSERT3S(uio_resid(uio), <=, zp->z_size);  // resid is smaller than file
-    ASSERT3S(uio_resid(uio) + uio_offset(uio), <=, zp->z_size); // both fit within file
+    //ASSERT3S(uio_resid(uio), <=, zp->z_size);  // resid is smaller than file
+    //ASSERT3S(uio_resid(uio) + uio_offset(uio), <=, zp->z_size); // both fit within file
     ASSERT3S(ubc_getsize(vp), ==, zp->z_size); // ought not to  be 0
 
     /* give the cluster layer a chance to fill in whatever data it already has */
@@ -798,8 +798,6 @@ mappedread(vnode_t *vp, int nbytes, struct uio *uio)
     off = upl_start & PAGE_MASK;
     upl_start &= ~PAGE_MASK;
     upl_size = (off + nbytes + (PAGE_SIZE - 1)) & ~PAGE_MASK;
-
-    ASSERT3S(upl_start + upl_size, >=, zp->z_size);
 
     dprintf("zfs_mappedread: %llu - %d (adj %llu - %llu)\n",
             uio_offset(uio), nbytes,
