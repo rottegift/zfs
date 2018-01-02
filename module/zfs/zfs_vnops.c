1fe9693d15869869c5141624c97ddb3a0bc00369
Sean Doran <smd@use.net>
2018-01-02 00:59:41 +0000

    printf->dprintf
    works as expected :)
    
    Jan  2 00:58:41 cla kernel[0]: ZFS: zfs_ubc_to_uio:1302: WARNING dirty page at index 0 (resid 262144, uio_resid 262144)uiooff 0 upl foff 0 sz 262144 fs ssdpool/smd/D2 file libzfs.2.dylib
    Jan  2 00:58:41 cla kernel[0]: ZFS: zfs_ubc_to_uio:1302: WARNING dirty page at index 1 (resid 258048, uio_resid 258048)uiooff 4096 upl foff 0 sz 262144 fs ssdpool/smd/D2 file libzfs.2.dylib
    Jan  2 00:58:41 cla kernel[0]: ZFS: zfs_ubc_to_uio:1302: WARNING dirty page at index 2 (resid 253952, uio_resid 253952)uiooff 8192 upl foff 0 sz 262144 fs ssdpool/smd/D2 file libzfs.2.dylib
    Jan  2 00:58:41 cla kernel[0]: ZFS: zfs_ubc_to_uio:1302: WARNING dirty page at index 3 (resid 249856, uio_resid 249856)uiooff 12288 upl foff 0 sz 262144 fs ssdpool/smd/D2 file libzfs.2.dylib
    Jan  2 00:58:41 cla kernel[0]: ZFS: zfs_ubc_to_uio:1302: WARNING dirty page at index 4 (resid 245760, uio_resid 245760)uiooff 16384 upl foff 0 sz 262144 fs ssdpool/smd/D2 file libzfs.2.dylib
    Jan  2 00:58:41 cla kernel[0]: ZFS: zfs_ubc_to_uio:1302: WARNING dirty page at index 5 (resid 241664, uio_resid 241664)uiooff 20480 upl foff 0 sz 262144 fs ssdpool/smd/D2 file libzfs.2.dylib
    Jan  2 00:58:41 cla kernel[0]: ZFS: zfs_ubc_to_uio:1302: WARNING dirty page at index 6 (resid 237568, uio_resid 237568)uiooff 24576 upl foff 0 sz 262144 fs ssdpool/smd/D2 file libzfs.2.dylib
    
    etc.

--- module/zfs/zfs_vnops.c
+++ module/zfs/zfs_vnops.c
@@ -1312,7 +1312,7 @@ zfs_ubc_to_uio(znode_t *zp, vnode_t *vp, struct uio *uio, int *bytes_to_copy,
 			 * it is OK to read this page, because it is already valid;
 			 * we do not change its dirty/busy state with our abort below
 			 */
-			printf("ZFS: %s:%d: WARNING dirty page at index %d (resid %d, uio_resid %lld)"
+			dprintf("ZFS: %s:%d: WARNING dirty page at index %d (resid %d, uio_resid %lld)"
 			    "uiooff %lld upl foff %lld sz %ld fs %s file %s\n",
 			    __func__, __LINE__, pg_index, resid, uio_resid(uio), uio_offset(uio),
 			    upl_file_offset, upl_size, fsname, fname);
