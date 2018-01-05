113fbb6a9243b051a893118bf9a3b77f1f357161
Sean Doran <smd@use.net>
2018-01-05 05:41:27 +0000

    fix bad comparison, and only ubc_setsize if sizes are different

--- module/zfs/zfs_vnops_osx.c
+++ module/zfs/zfs_vnops_osx.c
@@ -7346,16 +7346,21 @@ zfs_znode_getvnode(znode_t *zp, zfsvfs_t *zfsvfs)
 	zp->z_no_fsync = B_FALSE;
 
 	if (vnode_isreg(vp)) {
-		if (ubc_getsize(vp) != 0 && ubc_getsize != zp->z_size) {
+		if (ubc_getsize(vp) != 0 && ubc_getsize(vp) != zp->z_size) {
 			printf("ZFS: %s:%d: ubc_getsize %lld zp->z_size %lld file %s\n",
 			    __func__, __LINE__, ubc_getsize(vp),
 			    zp->z_size, zp->z_name_cache);
 		}
-		int set_initial_size_retval = ubc_setsize(vp, zp->z_size);
-		if (set_initial_size_retval == 0) {
-			// ubc_setsize returns TRUE on success, 0 on failure
-			printf("ZFS: %s:%d: unable to do initial ubc_setsize %lld for file %s\n",
-			    __func__, __LINE__, zp->z_size, zp->z_name_cache);
+
+		if (ubc_getsize(vp) != zp->z_size) {
+			int set_initial_size_retval = ubc_setsize(vp, zp->z_size);
+
+			if (set_initial_size_retval == 0) {
+				// ubc_setsize returns TRUE on success, 0 on failure
+				printf("ZFS: %s:%d: unable to do initial"
+				    " ubc_setsize %lld for file %s\n",
+				    __func__, __LINE__, zp->z_size, zp->z_name_cache);
+			}
 		}
 	}
 
