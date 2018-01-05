f3c610d065a8ea5b53300187510661a77a7599f0
Sean Doran <smd@use.net>
2018-01-05 04:35:07 +0000

    synchronize ubc size with z_size in zfs_znode_getvnode
    That leaves the question of whether all the million asserts from
    zfs_open having 0-sized ubc_getsize(*vpp) also needs a synchronization,
    which we'll know momentarily!

--- module/zfs/zfs_vnops_osx.c
+++ module/zfs/zfs_vnops_osx.c
@@ -7345,7 +7345,13 @@ zfs_znode_getvnode(znode_t *zp, zfsvfs_t *zfsvfs)
 
 	zp->z_no_fsync = B_FALSE;
 
-	ASSERT3S(zp->z_size, ==, ubc_getsize(vp));
+	ASSERT0(ubc_getsize(vp));
+	int set_initial_size_retval = ubc_setsize(vp, zp->z_size);
+	if (set_initial_size_retval == 0) {
+		// ubc_setsize returns TRUE on success, 0 on failure
+		printf("ZFS: %s:%d: unable to do initial ubc_setsize %lld for file %s\n",
+		    __func__, __LINE__, zp->z_size, zp->z_name_cache);
+	}
 
 	if (tsdrl) {
 		ASSERT3P(tsd_get(rl_key), ==, NULL);
