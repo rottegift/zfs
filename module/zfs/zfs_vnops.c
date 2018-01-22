564733df44ddd226300ddd937a4b8c17f1a17bd5
Sean Doran <smd@use.net>
2018-01-22 10:46:13 +0000

    Revert "deal with assertion"
    This reverts commit 55481dd5b7194b9fa7b4e183d8612c1fd8be186b.
    
    on reflection this will trigger other assertions

--- module/zfs/zfs_vnops.c
+++ module/zfs/zfs_vnops.c
@@ -2147,10 +2147,7 @@ zfs_write_maybe_extend_file(znode_t *zp, off_t woff, off_t start_resid, rl_t *rl
 			tsd_set(rl_key, rl);
 
 		if (ioflags & FAPPEND) {
-			if (ubc_getsize(vp) < zp->z_size + start_resid) {
-				int grow_setsize = ubc_setsize(vp, zp->z_size + start_resid);
-				ASSERT3U(grow_setsize, !=, 0); // true on success
-			}
+			ASSERT3U(zp->z_size + start_resid, ==, ubc_getsize(vp));
 		} else {
 			ASSERT3U(zp->z_size, ==, ubc_getsize(vp));
 		}
